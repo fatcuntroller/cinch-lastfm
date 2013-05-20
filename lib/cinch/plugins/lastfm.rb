2679d8b9e963e2b334b8fde86a7454a7dcda4c13
Anton Sebvie <thepossessedone@gmail.com>
2013-05-21 01:05:22 +0200

    Corrects some minor issues

--- lib/cinch/plugins/lastfm.rb
+++ lib/cinch/plugins/lastfm.rb
@@ -1,16 +1,17 @@
 require 'cinch'
-require 'lastfm'
+require 'nokogiri'
+require 'open-uri'
 
 module Cinch
 	module Plugins
 		module Lastfm
-			class Base
+			class Lastfm
 				include Cinch::Plugin
 				#attr_accessor :backend
 				# Redis backend
 
 				class << self
-					attr_accessor :lfmapi, :lfmsecret, #:host, :port # Redis backend scrub
+					attr_accessor :lfmapi, #:host, :port # Redis backend scrub
 
 					def configure(&block) 
 						yield self
@@ -19,7 +20,7 @@ module Cinch
 
 				def initialize(*args)
 					super
-					#@backend = Redis.new(self.class.host, self.class.port)
+					# @backend = Redis.new(self.class.host, self.class.port)
 					# Will feature a redis backend
 				end
 
@@ -28,6 +29,13 @@ module Cinch
 				# Gets Now Playing for an user
 				def now_playing(m, nick)
 
+				end
+
+				def get_data(nick)
+					lfmXML = Nokogiri::XML(open("http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=#{nick}&api_key=#{lfmapi}"))
+					artist = lfmXML.xpath('//artist').first.content
+					track = lfmXML.xpath('//name').first.content
+					return artist, track
 				end
 			end #Base
 		end #Lastfm
