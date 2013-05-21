8546be2c255744e83bddb20f6604ed8a56a48d66
Anton Sebvie <thepossessedone@gmail.com>
2013-05-21 19:12:34 +0200

    Prettyfies things a bit

--- lib/cinch/plugins/lastfm.rb
+++ lib/cinch/plugins/lastfm.rb
@@ -4,40 +4,38 @@ require 'open-uri'
 
 module Cinch
 	module Plugins
-		module Lastfm
-			class Lastfm
-				include Cinch::Plugin
-				#attr_accessor :backend
-				# Redis backend
+		class Lastfm
+			include Cinch::Plugin
+			#attr_accessor :backend
+			# Redis backend
 
-				class << self
-					attr_accessor :lfmapi, #:host, :port # Redis backend scrub
+			class << self
+				attr_accessor :lfmapi, #:host, :port # Redis backend scrub
 
-					def configure(&block) 
-						yield self
-					end
+				def configure(&block) 
+					yield self
 				end
+			end
 
-				def initialize(*args)
-					super
-					# @backend = Redis.new(self.class.host, self.class.port)
-					# Will feature a redis backend
-				end
+			def initialize(*args)
+				super
+				# @backend = Redis.new(self.class.host, self.class.port)
+				# Will feature a redis backend
+			end
 
-				match %r{np (.+?)}, :method => :now_playing
+			match %r{np (.+?)}, :method => :now_playing
 
-				# Gets Now Playing for an user
-				def now_playing(m, nick)
+			# Gets Now Playing for an user
+			def now_playing(m, nick)
 
-				end
+			end
 
-				def get_data(nick)
-					lfmXML = Nokogiri::XML(open("http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=#{nick}&api_key=#{lfmapi}"))
-					artist = lfmXML.xpath('//artist').first.content
-					track = lfmXML.xpath('//name').first.content
-					return artist, track
-				end
-			end #Base
+			def get_data(nick)
+				lfmXML = Nokogiri::XML(open("http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=#{nick}&api_key=#{lfmapi}"))
+				artist = lfmXML.xpath('//artist').first.content
+				track = lfmXML.xpath('//name').first.content
+				[artist, track]
+			end
 		end #Lastfm
 	end #Plugins
 end #Cinch
